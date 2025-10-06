e070a03474f2147bef93196e6a33d54a6d3aa9a5
gus <gavq0603qgmail.com>
2025-10-06 14:38:43 -0700

    Add user controller with create and fetch user functionality

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,55 @@
+import { postUser, getUserById } from "../services/UserService";
+import { Request, Response } from "express";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, lastname, username, password } = req.body;
+    if (!name || !lastname || !username || !password) {
+      return res.status(400).json({
+        error: "All fields are required (name, lastname, username, password)",
+      });
+    }
+
+    const newUser = await postUser({
+      name,
+      lastname,
+      username,
+      password,
+    });
+
+    const { password: _, ...userResponse } = newUser;
+
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: userResponse });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create user" });
+  }
+};
+
+const getUserByIdField = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const user = await getUserById(Number(id));
+
+    if (!user) {
+      return res.status(404).json({
+        error: "User not found",
+      });
+    }
+
+    const { password: _, ...userResponse } = user;
+
+    res.json({
+      message: "User found",
+      user: userResponse,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to fetch user",
+    });
+  }
+};
+
+export { createUser, getUserByIdField };
