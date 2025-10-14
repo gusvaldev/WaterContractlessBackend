3d3404a5586259b7ee32b6df1b98ad97627c0595
gus <gavq0603qgmail.com>
2025-10-13 22:03:43 -0700

    Remove VerificationCode export from models index

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,4 @@
 export { User } from "./User.js";
-export { VerificationCode } from "./VerificationCode.js";
 export { Subdivision } from "./SubdivisionModel.js";
 export { Street } from "./StreetModel.js";
 export { House } from "./HouseModel.js";
